b5631c3f179c5377066e88de76ad47d95e32baa2
sayyedaaman2 <sayyedaaman9@gmail.com>
2023-02-16 10:57:38 +0530

    complete webrtc demo

---
+++ client/pages/api/socket.ts
@@ -0,0 +1,63 @@
+import {Server} from 'socket.io'
+
+const Sockethandler = (req:any, res:any)=>{
+
+    if(res.socket.server.io){
+        console.log('Socket is already attached');
+        return res.end();
+    }
+
+    const io = new Server(res.socket.server);
+    res.socket.server.io = io;
+
+    io.on('connection', socket=>{
+        console.log(`user Connected : ${socket.id}`);
+
+
+        socket.on('join',(roomName)=>{
+            const {rooms} = io.sockets.adapter;
+            const room = rooms.get(roomName);
+
+            if(room == undefined){
+                socket.join(roomName);
+                socket.emit('created');
+
+            }else if(room.size === 1){
+                socket.join(roomName);
+                socket.emit('joined');
+            }else{
+                socket.emit('full');
+            }
+            console.log(rooms);
+        })
+
+        socket.on('ready',(roomName)=>{
+            socket.broadcast.to(roomName).emit('ready');
+        })
+
+
+        socket.on('ice-candidate',(candidate, roomName:string)=>{
+            console.log(candidate);
+            socket.broadcast.to(roomName).emit('ice-candidate',candidate);
+
+        });
+
+        socket.on('offer',(offer,roomName)=>{
+            socket.broadcast.to(roomName).emit('offer',offer);
+        })
+
+        
+
+        socket.on('answer',(answer,roomName)=>{
+            socket.broadcast.to(roomName).emit('answer',answer);
+        })
+
+        socket.on('leave',(roomName)=>{
+            socket.leave(roomName);
+            socket.broadcast.to(roomName).emit('leave');
+        })
+    })
+    return res.end();
+}
+
+export default Sockethandler;
